19b456b4d35b9654ed7cf48f1fb50c821b73d14d
Shreevatsa MS <shreevatsa.dush@gmail.com>
2021-06-03 21:50:27 +0530

    changed by deploy

--- src/main/java/com/mindtree/servlets/HomeServlet.java
+++ src/main/java/com/mindtree/servlets/HomeServlet.java
@@ -18,18 +18,7 @@ import com.mindtree.model.Employee;
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public HomeServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
